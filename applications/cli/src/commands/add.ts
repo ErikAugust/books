09bfbe0c891a324f5604194e013fd5ab493d1b34
Erik August Johnson <erik@erikaugust.com>
2021-06-22 11:03:33 -0400

    Adds ASCII art on command

--- applications/cli/src/commands/add.ts
+++ applications/cli/src/commands/add.ts
@@ -1,8 +1,14 @@
-import {Command, flags} from '@oclif/command';
+import { Command } from '@oclif/command';
 import { promptAddBook } from '../prompts/add';
 import { Books, Book } from '@eaj/books';
+import { displayAsciiArt } from '../misc/ascii';
+
 const chalk = require('chalk');
 
+/**
+ * @class Add
+ * @author Erik August Johnson <erik@eaj.io>
+ */
 export default class Add extends Command {
   static description = 'add a new book to list';
 
@@ -11,6 +17,7 @@ export default class Add extends Command {
     const books = new Books();
     books.list.push(new Book(book));
     books.save();
+    await displayAsciiArt('Books');
     this.log(`${chalk.bold.italic(book.title)} has been successfully added.`);
   }
 }
